a8f1f119a98a5056e367a1795c8c9f743d93168d
krubottom <karl.rubottom@gmail.com>
2017-04-24 20:40:49 -0600

    Updates

--- backup.ps1
+++ backup.ps1
@@ -1,4 +1,8 @@
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$date = Get-Date -Format MMddyyyy
 $source = "C:\ProgramData\Milestone"
-$destination = "C:\milestone.zip"
+$destination = "$DesktopPath\Milestone.$date.zip"
+
+# Add-Type -Path C:\Windows.old\WINDOWS\WinSxS\msil_system.io.compression.filesystem_b77a5c561934e089_4.0.14305.0_none_ac24209f3b58db7e\System.IO.Compression.FileSystem.dll
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compresion.zipfile]::CreateFromDirectory($source, $destination)
